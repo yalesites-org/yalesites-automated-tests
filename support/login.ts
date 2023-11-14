f212dcda45654fc0c4042434f864c6d5703f4768
David Blankenship <david.blankenship@yale.edu>
2023-11-14 10:38:56 -0500

    feat(search-tests): add search tests

---
+++ support/login.ts
@@ -0,0 +1,30 @@
+import { execSync, type ExecSyncOptions } from "child_process";
+
+const LOCAL_CMD = "lando drush uli";
+const REMOTE_CMD = "terminus drush ~ -- user:login";
+
+export default function getLoginUrl(path: string) : string {
+  let pathToUse = path;
+  if (!path) {
+    console.log("You might want to check the path is defined in YALESITES_PROJECT_PATH: defaulting to ../yalesites-project");
+    pathToUse = "../yalesites-project";
+  }
+
+  let cmd = LOCAL_CMD;
+  let opts: ExecSyncOptions = { cwd: pathToUse, stdio: 'pipe'};
+
+  // If the path is a reference to a pantheon name with .dev, .live, .test extension.
+  if (pathToUse.match(/\.dev|\.live|\.test/)) {
+    cmd = REMOTE_CMD.replace('~', pathToUse);
+    opts = { stdio: 'pipe' };
+  }
+
+  try {
+    const stdout = execSync(cmd, opts);
+    return stdout.toString().trim();
+  }
+  catch (err) {
+    console.log(err);
+    return null;
+  };
+};
