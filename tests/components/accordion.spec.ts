2303784eeeea343b172c44e86ea42166a54bb93e
David Blankenship <david.blankenship@yale.edu>
2024-02-13 09:44:19 -0500

    refactor(accordion): generalize tests across inputTypes

--- tests/components/accordion.spec.ts
+++ tests/components/accordion.spec.ts
@@ -1,27 +1,29 @@
 import { test, expect, type Page } from "@playwright/test";
 import { AxeBuilder } from '@axe-core/playwright';
 
+type InputType = "mouse" | "keyboard";
+
 test.beforeEach(async ({ page }) => {
   await page.goto("/component-pages-for-e2e-testing/accordion");
 });
 
-test.describe('with mouse', () => {
+const tests = (inputType: InputType) => {
   test("should open and close", async ({ page }) => {
     const accordionButtons = await page.$$(".accordion-item__toggle");
 
     for (const item of accordionButtons) {
-      await item.click();
+      await selectElement(page, item, inputType);
       expect(await item.getAttribute("aria-expanded")).toBe("true");
     };
 
     for (const item of accordionButtons) {
-      await item.click();
+      await selectElement(page, item, inputType);
       expect(await item.getAttribute("aria-expanded")).toBe("false");
     };
   });
 
   test("Expand All should open all accordions", async ({ page }) => {
-    await toggleAllAccordions(page);
+    await toggleAllAccordions(page, inputType);
     const accordionButtons = await page.$$(".accordion-item__toggle");
     for (const item of accordionButtons) {
       expect(await item.getAttribute("aria-expanded")).toBe("true");
@@ -30,14 +32,14 @@ test.describe('with mouse', () => {
 
   test("Collapse All should close all accordions", async ({ page }) => {
     // First open them all.
-    await toggleAllAccordions(page);
+    await toggleAllAccordions(page, inputType);
     const accordionButtons = await page.$$(".accordion-item__toggle");
     for (const item of accordionButtons) {
       expect(await item.getAttribute("aria-expanded")).toBe("true");
     };
 
     // Now close them all
-    await toggleAllAccordions(page);
+    await toggleAllAccordions(page, inputType);
     for (const item of accordionButtons) {
       expect(await item.getAttribute("aria-expanded")).toBe("false");
     };
@@ -48,7 +50,7 @@ test.describe('with mouse', () => {
   }) => {
     const accordionButtons = await page.$$(".accordion-item__toggle");
     const firstAccordion = accordionButtons[0];
-    await firstAccordion.click();
+    await selectElement(page, firstAccordion, inputType);
     const expandAllButton = await page.$(".accordion__toggle-all");
     expect(await expandAllButton?.innerText()).toBe("Expand All");
   });
@@ -58,78 +60,19 @@ test.describe('with mouse', () => {
   }) => {
     const accordionButtons = await page.$$(".accordion-item__toggle");
     for (const item of accordionButtons) {
-      await item.click();
+      await selectElement(page, item, inputType);
     };
     const collapseAllButton = await page.$(".accordion__toggle-all");
     expect(await collapseAllButton?.innerText()).toBe("Collapse All");
   });
+}
+
+test.describe('with mouse', () => {
+  tests("mouse");
 });
 
 test.describe('with keyboard', () => {
-  test("should open and close", async ({ page }) => {
-    const accordionButtons = await page.$$(".accordion-item__toggle");
-
-    expect(accordionButtons).toBeTruthy();
-    expect(accordionButtons.length).toBeGreaterThan(0);
-
-    for (const item of accordionButtons) {
-      await item.focus();
-      await page.keyboard.press("Enter");
-      expect(await item.getAttribute("aria-expanded")).toBe("true");
-    };
-
-    for (const item of accordionButtons) {
-      await item.focus();
-      await page.keyboard.press("Enter");
-      expect(await item.getAttribute("aria-expanded")).toBe("false");
-    };
-  });
-
-  test("Expand All should open all accordions", async ({ page }) => {
-    await toggleAllAccordions(page, "keyboard");
-    const accordionButtons = await page.$$(".accordion-item__toggle");
-    for (const item of accordionButtons) {
-      expect(await item.getAttribute("aria-expanded")).toBe("true");
-    };
-  });
-
-  test("Collapse All should close all accordions", async ({ page }) => {
-    // First open them all.
-    await toggleAllAccordions(page, "keyboard");
-    const accordionButtons = await page.$$(".accordion-item__toggle");
-    for (const item of accordionButtons) {
-      expect(await item.getAttribute("aria-expanded")).toBe("true");
-    };
-
-    // Now close them all
-    await toggleAllAccordions(page, "keyboard");
-    for (const item of accordionButtons) {
-      expect(await item.getAttribute("aria-expanded")).toBe("false");
-    };
-  });
-
-  test("If some accordions are open, ensure that the toggle button is set to Expand All", async ({
-    page,
-  }) => {
-    const accordionButtons = await page.$$(".accordion-item__toggle");
-    const firstAccordion = accordionButtons[0];
-    await firstAccordion.focus();
-    await page.keyboard.press("Enter");
-    const expandAllButton = await page.$(".accordion__toggle-all");
-    expect(await expandAllButton?.innerText()).toBe("Expand All");
-  });
-
-  test("If all accordions are manually expanded, ensure that the toggle button is set to Collapse All", async ({
-    page,
-  }) => {
-    const accordionButtons = await page.$$(".accordion-item__toggle");
-    for (const item of accordionButtons) {
-      await item.focus();
-      await page.keyboard.press("Enter");
-    };
-    const collapseAllButton = await page.$(".accordion__toggle-all");
-    expect(await collapseAllButton?.innerText()).toBe("Collapse All");
-  });
+  tests("keyboard");
 });
 
 test.describe('accessibility', () => {
@@ -139,14 +82,20 @@ test.describe('accessibility', () => {
   });
 });
 
-const toggleAllAccordions = async (page: Page, inputType = "mouse") => {
+const toggleAllAccordions = async (page: Page, inputType: InputType): Promise<void> => {
   const expandAllButton = await page.$(".accordion__toggle-all");
 
+  await selectElement(page, expandAllButton, inputType);
+};
+
+const selectElement = async (page: Page, element, inputType: InputType): Promise<void> => {
+  expect(inputType).toMatch(/mouse|keyboard/);
+
   if (inputType === "keyboard") {
-    await expandAllButton?.focus();
+    await element?.focus();
     await page.keyboard.press("Enter");
     return;
   }
 
-  await expandAllButton?.click();
+  await element?.click();
 };
