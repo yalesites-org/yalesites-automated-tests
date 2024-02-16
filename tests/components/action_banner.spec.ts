c2fdcab9136e01fa9029f6534044639535addd1f
David Blankenship <david.blankenship@yale.edu>
2024-02-16 12:04:37 -0500

    feat(action_banner): add action banner tests

--- tests/components/action_banner.spec.ts
+++ tests/components/action_banner.spec.ts
@@ -1,42 +1,45 @@
-import { expect, test, type Page, ElementHandle } from "@playwright/test";
-import tabKeyForBrowser from "support/tabKey";
-import a11yTests from "@support/a11yTests";
-import visRegTests from "@support/visRegTests";
+import { test } from "@playwright/test";
+import { expect } from "@support/axePage";
+import tabKeyForBrowser from "@support/tabKey";
 
-let tabKey: string;
-test.beforeEach(async ({ page, defaultBrowserType }) => {
-  // Webkit uses Alt+Tab over Tab to traverse, so we capture this before a test is done.
-  tabKey = tabKeyForBrowser(defaultBrowserType);
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
   await page.goto("/component-pages-for-e2e-testing/action-banner");
   await page.waitForLoadState("load");
-  page.locator("body");
 });
 
-const tests = (inputType: inputType) => {
-  test("inputType exists", async ({ page }) => {
-    expect(inputType).toBeDefined();
-  });
-};
-
-test.describe("with mouse", () => {
-  tests("mouse");
+test("should display heading", async ({ page }) => {
+  await expect(
+    page.getByRole("heading", { name: "Call to Action Banner" }),
+  ).toBeVisible();
 });
 
-test.describe("with keyboard", () => {
-  tests("keyboard");
-
-  test("can skip to main content", async ({ page, browserName }) => {
-    // Firefox can't target skip to main?  WHy?!?
-    if (browserName === "firefox") {
-      return;
-    }
-    await page.keyboard.press(tabKey);
-    const skipLink = page.locator("a[href='#main-content']");
-    await expect(skipLink).toBeFocused();
-    await expect(skipLink).toHaveText("Skip to main content");
-  });
+test("should display link", async ({ page }) => {
+  await expect(page.getByRole("link", { name: "External page" })).toBeVisible();
 });
 
-visRegTests();
+test("should display content", async ({ page }) => {
+  await expect(page.getByText("Banner content goes in here.")).toBeVisible();
+});
 
-a11yTests();
+test("should display background image", async ({ page }) => {
+  await expect(
+    page.getByRole("img", { name: "Two wooden chairs in an aisle" }),
+  ).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
