d9a97eb1ee7407f178c54d98639595e06e033787
David Blankenship <david.blankenship@yale.edu>
2024-02-19 12:51:49 -0500

    fix(script): just update the source

--- scripts/compareSites.sh
+++ scripts/compareSites.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-rm -rf playwright-report
-rm -rf tests-results
-rm -rf snapshots/all-vis-reg.spec.ts-snapshots
-YALESITES_URL="https://dev-yalesites-mv482.pantheonsite.io" npx playwright test all-vis-reg.spec --reporter=dot
-YALESITES_URL="https://d10-yalesites-mv482.pantheonsite.io" npx playwright test all-vis-reg.spec --reporter=html
-npx playwright show-report
+runTests() {
+    site1=${1:-"https://dev-yalesites-mv482.pantheonsite.io"}
+    site2=${2:-"https://d10-yalesites-mv482.pantheonsite.io"}
+
+    YALESITES_URL="$site1" npx playwright test all-vis-reg.spec --reporter=dot --update-snapshots
+    YALESITES_URL="$site2" npx playwright test all-vis-reg.spec --reporter=html
+    npx playwright show-report
+}
+
+runTests "$@"
