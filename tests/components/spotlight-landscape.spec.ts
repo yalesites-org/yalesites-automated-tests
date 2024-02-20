a714ee788c582aeb765e6dfe81ec4b7d17d1fabc
David Blankenship <david.blankenship@yale.edu>
2024-02-20 14:42:57 -0500

    feat(spotlight-landscape): add spotlight-landscape tests

---
+++ tests/components/spotlight-landscape.spec.ts
@@ -0,0 +1,42 @@
+import { test } from "@playwright/test";
+import { expect } from "@support/axePage";
+import tabKeyForBrowser from "@support/tabKey";
+
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
+  await page.goto("/component-pages-for-e2e-testing/spotlight-landscape");
+  await page.waitForLoadState("load");
+});
+
+test("has the first spotlight", async ({ page }) => {
+  await expect(page.getByRole('heading', { name: 'Spotlight - Landscape Heading' })).toBeVisible();
+  await expect(page.getByRole('img', { name: 'Olde English Bulldog sitting' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Spotlight - Landscape Heading' })).toBeVisible();
+  await expect(page.getByText('Spotlight - Landscape Subheading')).toBeVisible();
+  await expect(page.getByText('Lorem ipsum dolor sit amet')).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Example Page (link is external)', exact: true })).toBeVisible();
+});
+
+test("has the second spotlight", async ({ page }) => {
+  await expect(page.getByRole('heading', { name: 'Spotlight 2 Heading' })).toBeVisible();
+  await expect(page.getByText('Spotlight 2 subheading')).toBeVisible();
+  await expect(page.getByText('Lorem ipsum dolor sit example')).toBeVisible();
+  await expect(page.getByRole('img', { name: 'Towering shelves of books in' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Example Document (file' })).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
