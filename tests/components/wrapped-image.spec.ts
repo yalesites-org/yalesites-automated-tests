a12e7f266fd2177c8bcbd0329b176303838a95d7
David Blankenship <david.blankenship@yale.edu>
2024-02-20 16:14:34 -0500

    feat(wrapped-image): add wrapped-image tests

---
+++ tests/components/wrapped-image.spec.ts
@@ -0,0 +1,39 @@
+import { test } from "@playwright/test"; import { expect } from "@support/axePage"; import tabKeyForBrowser from "@support/tabKey";
+
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
+  await page.goto("/component-pages-for-e2e-testing/wrapped-image");
+  await page.waitForLoadState("load");
+});
+
+test("has a heading", async ({ page }) => {
+  await expect(page.getByRole('heading', { name: 'Wrapped Image' })).toBeVisible();
+});
+
+test("has an image", async ({ page }) => {
+  await expect(page.getByRole('img', { name: 'Looking down on a spiral' })).toBeVisible();
+});
+
+test("has a paragraph", async ({ page }) => {
+  await expect(page.getByText('Lorem example page(link is external) sit example doc (PDF)(file download)')).toBeVisible();
+  await expect(page.getByText('Lorem example page(link is external) consectetur adipiscing elit, habitasse')).toBeVisible();
+  await expect(page.getByText('Mauris praesent himenaeos')).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
+
+
