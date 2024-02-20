3932931b15e77cfa8d407b1286e7d7a27008e91e
David Blankenship <david.blankenship@yale.edu>
2024-02-20 10:35:53 -0500

    feat(divider): add divider tests

---
+++ tests/components/divider.spec.ts
@@ -0,0 +1,29 @@
+import { test } from "@playwright/test";
+import { expect } from "@support/axePage";
+import tabKeyForBrowser from "@support/tabKey";
+
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
+  await page.goto("/component-pages-for-e2e-testing/divider");
+  await page.waitForLoadState("load");
+});
+
+test("exists on the page", async ({ page }) => {
+  await expect(page.locator('.divider')).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
