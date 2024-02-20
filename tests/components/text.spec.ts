e29dccf7bd0517aad7bd0f2b9ddae622fb2185fe
David Blankenship <david.blankenship@yale.edu>
2024-02-20 14:55:59 -0500

    feat(text): add text tests

---
+++ tests/components/text.spec.ts
@@ -0,0 +1,27 @@
+import { test } from "@playwright/test"; import { expect } from "@support/axePage"; import tabKeyForBrowser from "@support/tabKey";
+
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
+  await page.goto("/component-pages-for-e2e-testing/text");
+  await page.waitForLoadState("load");
+});
+
+test("has text", async ({ page }) => {
+  await expect(page.getByText('Lorem ipsum example page(link')).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
