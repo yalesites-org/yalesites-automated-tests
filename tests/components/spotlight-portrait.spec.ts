20022232ff34b4d019be5a19f733b006d77e832b
David Blankenship <david.blankenship@yale.edu>
2024-02-20 14:46:14 -0500

    feat(spotlight-portrait): add spotlight-portrait tests

---
+++ tests/components/spotlight-portrait.spec.ts
@@ -0,0 +1,44 @@
+import { test } from "@playwright/test";
+import { expect } from "@support/axePage";
+import tabKeyForBrowser from "@support/tabKey";
+
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
+  await page.goto("/component-pages-for-e2e-testing/spotlight-portrait");
+  await page.waitForLoadState("load");
+});
+
+test("has the first spotlight", async ({ page }) => {
+  await expect(page.locator('#main-content div').filter({ hasText: 'Spotlight - Portrait Heading' }).nth(3)).toBeVisible();
+  await expect(page.getByRole('img', { name: 'Olde English Bulldog sitting' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Spotlight - Portrait Heading' })).toBeVisible();
+  await expect(page.getByText('Spotlight - Portrait Subheading')).toBeVisible();
+  await expect(page.getByText('Lorem ipsum dolor sit amet').first()).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Example Page (link is external)', exact: true })).toBeVisible();
+
+});
+
+test("has the second spotlight", async ({ page }) => {
+  await expect(page.locator('#main-content div').filter({ hasText: 'Spotlight - Portrait 2 Heading Spotlight - Portrait 2 Subheading Lorem ipsum' }).nth(2)).toBeVisible();
+  await expect(page.getByRole('img', { name: 'A lion-headed gargoyle' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Spotlight - Portrait 2 Heading' })).toBeVisible();
+  await expect(page.getByText('Spotlight - Portrait 2 Subheading')).toBeVisible();
+  await expect(page.getByText('Lorem ipsum dolor sit amet').nth(1)).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Example Word Document (file' })).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
