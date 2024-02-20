522c61d429028b003ce3d4672c432bd66dae23f0
David Blankenship <david.blankenship@yale.edu>
2024-02-20 13:26:49 -0500

    feat(embed): add embed tests

---
+++ tests/components/embed.spec.ts
@@ -0,0 +1,30 @@
+import { test } from "@playwright/test";
+import { expect } from "@support/axePage";
+import tabKeyForBrowser from "@support/tabKey";
+
+let tabKey = "Tab";
+test.beforeEach(async ({ page, browserName }) => {
+  tabKey = tabKeyForBrowser(browserName);
+  await page.goto("/component-pages-for-e2e-testing/embed");
+  await page.waitForLoadState("load");
+});
+
+test("has an iframe", async ({ page }) => {
+  await page.waitForTimeout(500);
+  await expect(page.locator('iframe[title="X Post"]')).toBeVisible();
+});
+
+test("visual regression should match previous screenshot", async ({ page }) => {
+  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixels: 100 });
+});
+
+test("should pass axe", async ({ page }) => {
+  const axe_tags = [
+    "wcag2a",
+    "wcag2aa",
+    "wcag21a",
+    "wcag21aa",
+    "best-practice",
+  ];
+  await expect(page).toPassAxe({ tags: axe_tags });
+});
