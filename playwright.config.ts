33d51000b5d5405fc352183e7da7e2e121114792
David Blankenship <david.blankenship@yale.edu>
2024-02-23 14:27:59 -0500

    feat(playwright): default back to html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "line",
+  reporter: "html",
   timeout: 60000,
   // Change the location of snapshots so that they aren't in our test folder
   snapshotDir: "./snapshots",
