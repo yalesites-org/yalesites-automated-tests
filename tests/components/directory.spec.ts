db3d1adfe08b6c9cabc968ca474899fd652aa29c
David Blankenship <david.blankenship@yale.edu>
2024-02-26 14:09:38 -0500

    feat(directory): add email test
    The email in a directory is now labelled "Email", and is a link with a
    mailto to their entered email address.  This tests against that.

--- tests/components/directory.spec.ts
+++ tests/components/directory.spec.ts
@@ -1,6 +1,9 @@
 import { test } from "@playwright/test";
 import { expect } from "@support/axePage";
-import { pressKeyForBrowser, type PressKeyForBrowserFunction } from "@support/tabKey";
+import {
+  pressKeyForBrowser,
+  type PressKeyForBrowserFunction,
+} from "@support/tabKey";
 
 let pressTabKeyRepeatedly: PressKeyForBrowserFunction;
 test.beforeEach(async ({ page, browserName, isMobile }) => {
@@ -10,40 +13,52 @@ test.beforeEach(async ({ page, browserName, isMobile }) => {
 });
 
 test("has a heading", async ({ page }) => {
-  await expect(page.getByRole('heading', { name: 'Directory Heading' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Directory Heading" }),
+  ).toBeVisible();
 });
 
 test("has an affiliation label", async ({ page }) => {
-  await expect(page.getByText('Affiliation')).toBeVisible();
+  await expect(page.getByText("Affiliation")).toBeVisible();
 });
 
 test("has an affiliation drop down", async ({ page }) => {
-  await expect(page.getByLabel('Affiliation')).toBeVisible();
+  await expect(page.getByLabel("Affiliation")).toBeVisible();
 });
 
 test("has an apply button", async ({ page }) => {
-  await expect(page.getByRole('button', { name: 'Apply' })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Apply" })).toBeVisible();
 });
 
 test("has a directory entry", async ({ page }) => {
-  await expect(page.getByRole('img', { name: 'Portrait of Tom Foolery' })).toBeVisible();
-  await expect(page.getByText('Information Technology')).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Tom Foolery' })).toBeVisible();
-  await expect(page.getByText('Chief Silliness Officer')).toBeVisible();
-  await expect(page.getByText('Deputy CIO')).toBeVisible();
-  await expect(page.getByText('tom.foolery@yale.edu')).toBeVisible();
-  await expect(page.getByText('-432-0987')).toBeVisible();
+  await expect(
+    page.getByRole("img", { name: "Portrait of Tom Foolery" }),
+  ).toBeVisible();
+  await expect(page.getByText("Information Technology")).toBeVisible();
+  await expect(page.getByRole("link", { name: "Tom Foolery" })).toBeVisible();
+  await expect(page.getByText("Chief Silliness Officer")).toBeVisible();
+  await expect(page.getByText("Deputy CIO")).toBeVisible();
+  await expect(page.getByRole("link", { name: "Email" })).toBeVisible();
+  await expect(page.getByText("-432-0987")).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "Email" })).toHaveAttribute(
+    "href",
+    "mailto:tom.foolery@yale.edu",
+  );
 });
 
-test("can select an affiiliation", async ({ page }) => {
-  await page.getByLabel('Affiliation').selectOption('32');
-  await page.getByRole('button', { name: 'Apply' }).click();
+test("can select an affiliation", async ({ page }) => {
+  await page.getByLabel("Affiliation").selectOption("32");
+  await page.getByRole("button", { name: "Apply" }).click();
   await page.waitForLoadState("load");
-  await expect(page.getByLabel('Affiliation')).toHaveValue('32');
+  await expect(page.getByLabel("Affiliation")).toHaveValue("32");
 });
 
 test("visual regression should match previous screenshot", async ({ page }) => {
-  await expect(page).toHaveScreenshot({ fullPage: true, maxDiffPixelRatio: 0.17 });
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    maxDiffPixelRatio: 0.17,
+  });
 });
 
 test("should pass axe", async ({ page }) => {
