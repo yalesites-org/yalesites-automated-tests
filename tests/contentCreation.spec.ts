0b81d9866a5c9d04da39474131333fac62c66c55
David Blankenship <david.blankenship@yale.edu>
2024-03-11 11:50:57 -0400

    feat(YSP-409): create event support

--- tests/contentCreation.spec.ts
+++ tests/contentCreation.spec.ts
@@ -4,5 +4,18 @@ import { createContentType } from "@support/drupalPage";
 test.use({ storageState: 'playwright/.auth/siteAdmin.json' });
 
 test("can create a new page", async ({ page }) => {
-  expect(await createContentType(page, "page", { title: "My new page" })).toBe(true);
+  expect(await createContentType(page, "page", { Title: "My new page", "Teaser Title": "Hi" })).toBe(true);
+  expect(await page.title()).toBe("Edit layout for My new page | YaleSites");
+});
+
+test("can create a new event", async ({ page }) => {
+  expect(await createContentType(page, "event", {
+    Title: "My new event",
+    "In-person": true,
+    "#edit-field-event-date-0-time-wrapper-value-date": new Date(),
+    "#edit-field-event-date-0-time-wrapper-value-time": "12:00 PM",
+    "#edit-field-event-date-0-time-wrapper-end-value-date": new Date(),
+    "#edit-field-event-date-0-time-wrapper-end-value-time": "3:00 PM",
+  })).toBe(true);
+  expect(await page.title()).toBe("Edit layout for My new event | YaleSites");
 });
