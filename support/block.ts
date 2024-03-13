89e3e42e02fb490a85b294a3446d9ce7679cc42f
David Blankenship <david.blankenship@yale.edu>
2024-03-13 09:03:39 -0400

    docs(blocks): add comments to methods

--- support/block.ts
+++ support/block.ts
@@ -1,6 +1,16 @@
 import { type Page, type Locator } from '@playwright/test';
 
-type BlockType = "text" | "image" | "video" | "wrapped_image" | "quote" | "button_link" | "callout" | "divider" | "spotlight_landscape" | "spotlight_portrait" | "quick_links";
+type BlockType = "text"
+  | "image"
+  | "video"
+  | "wrapped_image"
+  | "quote"
+  | "button_link"
+  | "callout"
+  | "divider"
+  | "spotlight_landscape"
+  | "spotlight_portrait"
+  | "quick_links";
 
 interface Block {
   text: {
@@ -121,12 +131,52 @@ interface Block {
   },
 };
 
-const createBlock = async (page: Page, blockType: BlockType, block: Partial<Block[BlockType]>) => {
+/*
+ * Create a block
+ * Note: This assumes you're already logged in and on the page you want to
+ * create a block on the page you want to create a block on.
+ *
+ * @param page - the Playwright page
+ * @param blockType - the type of block to create
+ * @param block - the block to create
+ *
+ * @returns true if the block was created, false if not
+ *
+ * @example
+ * createBlock(page, "text", {
+ *  administrative_label: "My new text block",
+ *  content: "This is a test",
+ *  text_style_variation: "Default",
+ *  reusable_block: false,
+ *  });
+ *
+ *  createBlock(page, "quick_links", {
+ *  administrative_label: "My new quick links block",
+ *  quick_links_component_title: "Quick Links",
+ *  quick_links: [
+ *  { url: "https://yalesites.yale.edu", link_text: "YaleSites main page" },
+ *  { url: "https://google.com", link_text: "Google" },
+ *  { url: "https://yahoo.com", link_text: "Yahoo" },
+ *  ],
+ *  reusable_block: false,
+ *  });
+ */
+const createBlock = async (
+  page: Page,
+  blockType: BlockType,
+  block: Partial<Block[BlockType]>) => {
   const blockTypeLabel = humanize(blockType)
 
-  await page.getByRole('link', { name: 'Add block in Content Section' }).last().click({ force: true });
+  await page
+    .getByRole('link', { name: 'Add block in Content Section' })
+    .last()
+    .click({ force: true });
+  // Would love to not have to use locator here.  Fix if you can.
   const layoutBuilderModal = page.locator('div#layout-builder-modal');
-  await layoutBuilderModal.getByRole('link', { name: blockTypeLabel }).first().click();
+  await layoutBuilderModal
+    .getByRole('link', { name: blockTypeLabel })
+    .first()
+    .click();
 
   await fillInForm(page, block);
 
@@ -135,13 +185,63 @@ const createBlock = async (page: Page, blockType: BlockType, block: Partial<Bloc
   return true;
 };
 
-const fillInForm = async (page: Page, elements: { [key: string]: any }, index: number = 0) => {
+/*
+ * Fill in a form
+ *
+ * @param page - the Playwright page
+ * @param elements - the elements to fill in
+ * @param index - the index of the element to fill in in the event we have
+ *   multiple items (think subitems like quick link links)
+ *
+ * @returns void
+ *
+ * @example
+ * fillInForm(page, {
+ * Title: "My new page",
+ * "Teaser Title": "Hi",
+ * });
+ */
+const fillInForm = async (
+  page: Page,
+  elements: { [key: string]: any },
+  index: number = 0) => {
   for (const key in elements) {
     await fillInFormElement(page, key, elements[key], index);
   }
 }
 
-const fillInFormElement = async (page: Page, key: string, value: any, index: number = 0) => {
+/*
+ * Fill in a form element
+ *
+ * @param page - the Playwright page
+ * @param key - the key of the element to fill in
+ * @param value - the value to fill in
+ * @param index - the index of the element to fill in in the event we have
+ *  multiple items (think subitems like quick link links)
+ *
+ *  @returns void
+ *
+ *  @example
+ *
+ *  fillInFormElement(page, "Title", "My new page");
+ *
+ *  fillInFormElement(page, "Teaser Title", "Hi");
+ *
+ *  fillInFormElement(page, "quick_links", [
+ *  { url: "https://yalesites.yale.edu", link_text: "YaleSites main page" },
+ *  { url: "https://google.com", link_text: "Google" },
+ *  { url: "https://yahoo.com", link_text: "Yahoo" },
+ *  ]);
+ *
+ *  Note: in the above case with an array of values, the "quick_links" key is
+ *  not used.  It is a placeholder name so users can understand what they will
+ *  be.
+ */
+const fillInFormElement = async (
+  page: Page,
+  key: string,
+  value: any,
+  index: number = 0) => {
   if (value instanceof Array) {
     for (let index = 0; index < value.length; index++) {
       await fillInForm(page, value[index], index);
@@ -154,7 +254,8 @@ const fillInFormElement = async (page: Page, key: string, value: any, index: num
     await fillInForm(page, value, 0);
   } else {
     let label = humanize(key);
-    // If label contains content, rename it to target 'Editor editing area: main'
+    // If label contains content, rename it to target
+    // 'Editor editing area: main'
     if (label.includes('Content')) {
       label = 'Editor editing area: main';
     }
@@ -165,8 +266,23 @@ const fillInFormElement = async (page: Page, key: string, value: any, index: num
   }
 };
 
+/*
+ * Humanize a string
+ *
+ * @param str - the string to humanize
+ *
+ * @returns the humanized string
+ *
+ * @example
+ *
+ * humanize("quick_links") // "Quick Links"
+ * humanize("quick_links_component_title") // "Quick Links Component Title"
+ */
 const humanize = (str: string) => {
-  return str.split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  return str
+    .split('_')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 };
 
 const fillAny = async (element: Locator, value: any) => {
--- tests/blockCreation.spec.ts
+++ tests/blockCreation.spec.ts
@@ -1,46 +1,44 @@
-import { expect, test, type Page } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { createContentType } from "@support/drupalPage";
 import { createBlock } from "@support/block";
 
 test.use({ storageState: 'playwright/.auth/siteAdmin.json' });
 
-test("can create a new text block", async ({ page, isMobile }) => {
-  if (!isMobile) {
-    expect(await createContentType(page, "page", { Title: "My new page" })).toBe(true);
-    expect(await createBlock(page, "text", {
-      administrative_label: "My new text block",
-      content: "This is a test",
-      text_style_variation: "Default",
-      reusable_block: false,
-    })).toBe(true);
-    await page.getByRole('button', { name: 'Save', exact: true }).click();
-    expect(await page.title()).toBe("My new page | YaleSites");
-    expect(page.getByRole('paragraph')).toHaveText('This is a test');
-  } else {
+test.beforeEach(async ({ isMobile }) => {
+  if (isMobile) {
     test.skip();
   }
 });
 
-test("can create a new quick links block", async ({ page, isMobile }) => {
-  if (!isMobile) {
-    expect(await createContentType(page, "page", { Title: "My new quick links page" })).toBe(true);
-    expect(await createBlock(page, "quick_links", {
-      administrative_label: "My new quick links block",
-      quick_links_component_title: "Quick Links",
-      quick_links: [
-        { url: "https://yalesites.yale.edu", link_text: "YaleSites main page" },
-        { url: "https://google.com", link_text: "Google" },
-        { url: "https://yahoo.com", link_text: "Yahoo" },
-      ],
-      reusable_block: false,
-    })).toBe(true);
-    await page.getByRole('button', { name: 'Save', exact: true }).click();
-    await expect(page).toHaveTitle("My new quick links page | YaleSites");
-    await expect(page.getByRole('heading', { name: 'Quick Links', exact: true })).toHaveText('Quick Links');
-    await expect(page.getByRole('link', { name: 'YaleSites main page' })).toHaveAttribute('href', 'https://yalesites.yale.edu');
-    await expect(page.getByRole('link', { name: 'Google' })).toHaveAttribute('href', 'https://google.com');
-    await expect(page.getByRole('link', { name: 'Yahoo' })).toHaveAttribute('href', 'https://yahoo.com');
-  } else {
-    test.skip();
-  }
+test("can create a new text block", async ({ page }) => {
+  expect(await createContentType(page, "page", { Title: "My new page" })).toBe(true);
+  expect(await createBlock(page, "text", {
+    administrative_label: "My new text block",
+    content: "This is a test",
+    text_style_variation: "Default",
+    reusable_block: false,
+  })).toBe(true);
+  await page.getByRole('button', { name: 'Save', exact: true }).click();
+  expect(await page.title()).toBe("My new page | YaleSites");
+  expect(page.getByRole('paragraph')).toHaveText('This is a test');
+});
+
+test("can create a new quick links block", async ({ page }) => {
+  expect(await createContentType(page, "page", { Title: "My new quick links page" })).toBe(true);
+  expect(await createBlock(page, "quick_links", {
+    administrative_label: "My new quick links block",
+    quick_links_component_title: "Quick Links",
+    quick_links: [
+      { url: "https://yalesites.yale.edu", link_text: "YaleSites main page" },
+      { url: "https://google.com", link_text: "Google" },
+      { url: "https://yahoo.com", link_text: "Yahoo" },
+    ],
+    reusable_block: false,
+  })).toBe(true);
+  await page.getByRole('button', { name: 'Save', exact: true }).click();
+  await expect(page).toHaveTitle("My new quick links page | YaleSites");
+  await expect(page.getByRole('heading', { name: 'Quick Links', exact: true })).toHaveText('Quick Links');
+  await expect(page.getByRole('link', { name: 'YaleSites main page' })).toHaveAttribute('href', 'https://yalesites.yale.edu');
+  await expect(page.getByRole('link', { name: 'Google' })).toHaveAttribute('href', 'https://google.com');
+  await expect(page.getByRole('link', { name: 'Yahoo' })).toHaveAttribute('href', 'https://yahoo.com');
 });
